a4eca8a99a2040710e30cdc9db6fbf92676f809f
unknown <piotrek.korzeniewski@wp.pl>
2023-11-01 19:32:35 +0100

    Update colors

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -8,7 +8,7 @@ void main(){
 
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(Color.fromARGB(255, 94, 4, 108),Color.fromARGB(255, 107, 0, 119)),
+        body: GradientContainer(Color.fromARGB(255, 89, 3, 103),Color.fromARGB(255, 107, 0, 119)),
       ),
     ),
 
